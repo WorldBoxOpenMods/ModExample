ef5b0ec22803c006f8f60c40d16d52a897e4d595
inmny <1342885270@qq.com>
2023-11-29 20:37:18 +0800

    Feature: Add reload example

--- ExampleModCode.cs
+++ ExampleModCode.cs
@@ -1,16 +1,39 @@
 using NeoModLoader.api;
 using ExampleMod.UI;
 using ExampleMod.Content;
+using NeoModLoader.api.attributes;
+
 namespace ExampleMod;
 
-public class ExampleModMain : BasicMod<ExampleModMain>{
+public class ExampleModMain : BasicMod<ExampleModMain>, IReloadable{
     protected override void OnModLoad(){
         LogInfo("Hello World!");
         ExampleGodPowers.init();
         ExampleTab.Init();
         ExampleTraits.Init();
+        Reload();
     }
     public static void Called(){
         LogInfo("Hello World From Another!");
+    }
+
+    public void Reload()
+    {
+        ExampleHotfixClass.HotfixExample();
+        HotfixExample();
+    }
+    [Hotfixable]
+    public static void HotfixExample()
+    {
+        LogInfo("Hello World after Hotfix! at 2 from Main");
+    }
+}
+
+public class ExampleHotfixClass
+{
+    [Hotfixable]
+    public static void HotfixExample()
+    {
+        ExampleModMain.LogInfo("Hello World after Hotfix! at 2");
     }
 }
