b2eb71c3f57da5d0ab84cf7e6803915e8dfa13ad
inmny <1342885270@qq.com>
2023-12-02 19:55:42 +0800

    Feature: Add example of lambda function hotfix

--- ExampleModCode.cs
+++ ExampleModCode.cs
@@ -32,8 +32,20 @@ public class ExampleModMain : BasicMod<ExampleModMain>, IReloadable{
         // 实现了重载切换ExampleTrait的action_special_effect
         _reload_switch = !_reload_switch;
         var example_trait_to_update = AssetManager.traits.get("ExampleTrait");
-        example_trait_to_update.action_special_effect = _reload_switch ? ExampleActions.example_trait_special_effect : null;
+        example_trait_to_update.action_special_effect = _reload_switch
+            ? ExampleActions.example_trait_special_effect
+            : [Hotfixable](pTarget, pTile) =>
+            {
+                // 用于重载的匿名函数示例
+                if (pTarget != null) return false;
+                // You can modify the method when game running. And click the reload button in the mod menu to reload the method.
+                // 你可以在游戏运行时修改方法，然后点击mod菜单中的reload按钮来重新加载方法。
+                ExampleModMain.LogInfo($"lambda function called");
+                return true;
+            };
         example_trait_to_update.special_effect_interval = 1f;
+        // 重载匿名函数示例
+        if (!_reload_switch) example_trait_to_update.action_special_effect(null, null);
         
         // 将特质的效果更新即时应用于所有单位. 
         foreach(var actor in World.world.units){
